7bcbe376b35930a3ea8302439c0a62aa709c4bbe
Khrystyna Mykolaichuk <xrustyaxl@ukr.net>
2022-11-06 18:19:32 +0200

    Add App, SharedLayout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout';
+import { Cast } from './Cast/Cast';
+import { Reviews } from './Reviews/Reviews';
+
+const createAsyncComponent = path => lazy(() => import(path));
+
+const Movies = createAsyncComponent('../pages/Movies');
+const Home = createAsyncComponent('../pages/Home');
+const MovieDetails = createAsyncComponent('../pages/MovieDetails');
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />} />
+        <Route path="cast" element={<Cast />} />
+        <Route path="reviews" element={<Reviews />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 };
